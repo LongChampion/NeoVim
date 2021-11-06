06248f624e2e7ddb3328cb3aa93a39807f87050a
LongChampion <ThePalazin@Gmail.com>
2021-11-06 14:51:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,21 @@ if [[ $EUID -eq 0 ]]; then
     exit 1
 fi
 
+YAY="yay -S --noconfirm --needed"
+DEPENDANCY="neovim git nodejs npm clang fzf ttf-font-awesome awesome-terminal-fonts nerd-fonts-fira-code"
+
+$YAY $DEPENDANCY
+
 if [[ ! -d $HOME/.config/nvim ]]; then
     git clone --depth 1 https://github.com/LongChampion/NeoVim $HOME/.config/nvim
     curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    nvim +PlugInstall +qall
+    nvim +PlugInstall +qa
+    
+    # Complete coc.nvim setup
+    sudo npm install -g yarn
+    cd $HOME/.config/nvim/plugged/coc.nvim
+    yarn install
+else
+    echo "$HOME/.config/nvim already exists!"
+    echo "Backup it then rerun this script!"
 fi
